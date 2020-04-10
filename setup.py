fbcadc0280d83a952f6f2e4392ebeaa3301a1c44
Sarath Kaul <kaul.sarath@gmail.com>
2020-04-10 07:17:37 +0000

    Package Version Changes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 setup(
   name="medipy",
-  version="0.0.1",
+  version="0.0.2",
   description="",
   long_description=README,
   long_description_content_type="text/markdown",
